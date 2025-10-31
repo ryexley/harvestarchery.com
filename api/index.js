ce9f302fe8a26c2d5376268982a7974596a1ff35
Bob Yexley <bob@yexley.net>
2025-10-31 09:09:22 -0400

    chore: still trying to get build and deploy working

--- api/index.js
+++ api/index.js
@@ -3,3 +3,7 @@ const { createRequestHandler } = require("@remix-run/vercel");
 module.exports = createRequestHandler({
   build: require("./_build")
 });
+
+module.exports.config = {
+  runtime: 'nodejs'
+};
