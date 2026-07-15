6bdca373cdb48651bb9d4bbaea8074266243f459
Bob Yexley <bob@yexley.net>
2026-07-15 10:20:50 -0400

    chore: updates for fall primer

--- app/data/events.js
+++ app/data/events.js
@@ -2,10 +2,10 @@ import isBefore from "date-fns/isBefore"
 
 export const events = [
 	{
-		title: "The Harvest Spring Primer at Quarry Rock",
-		date: "April 18th & 19th (Saturday & Sunday)",
+		title: "The Harvest Fall Primer at Quarry Rock",
+		date: "August 14th – 16th (Friday through Sunday)",
 		time: "8:00am to 4:00pm each day",
 		detailsKey: "primer",
-		published: isBefore(Date.now(), new Date("2026-04-20T03:59:59.000Z")),
+		published: isBefore(Date.now(), new Date("2026-08-16T03:59:59.000Z")),
 	}
 ]
--- app/routes/events_.primer_.faq.jsx
+++ app/routes/events_.primer_.faq.jsx
@@ -13,7 +13,7 @@ import { IMAGE_TYPE } from "~/util/images"
 import { styled, keyframes, breakpointPx as sizes } from "~/styles"
 
 export const meta = () => ([
-	{ title: windowTitle(`Frequently Asked Questions | The Harvest Spring Primer at Quarry Rock`) }
+	{ title: windowTitle(`Frequently Asked Questions | The Harvest Fall Primer at Quarry Rock`) }
 ])
 
 export async function loader({ request }) {
@@ -258,7 +258,7 @@ export default function TheRockArcheryChallengePage() {
 					<Hero imageBoxProps={heroProps} style={{ marginTop: "auto" }} />
 				</HeroImage>
 				<HeroContent>
-					<PageHeading>The Harvest Spring Primer at Quarry Rock</PageHeading>
+					<PageHeading>The Harvest Fall Primer at Quarry Rock</PageHeading>
 					<SubHeading>Frequently Asked Questions</SubHeading>
 				</HeroContent>
 			</HeroWrapper>
